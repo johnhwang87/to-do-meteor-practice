c824cbf1c6d04896929dd7e4b4016bfb7ef0f031
John Hwang <johnsies87@gmail.com>
2016-02-29 18:05:12 -0800

    first commit to meteor tutorial

---
+++ resolutions.js
@@ -0,0 +1,61 @@
+// Creating a new collection for the mongo database
+Resolutions = new Mongo.Collection('resolutions');
+
+// runs on client startup
+if (Meteor.isClient) {
+  // template helper allows you to use information on the page
+  Template.body.helpers({
+    // calling all resolutions from the database
+    resolutions: function() {
+      return Resolutions.find();
+    }
+  });
+  // Calling events into your app
+  // this is named .body because it is not in the resolutio template
+  Template.body.events({
+    // string which contains the event itself = submit
+    // second part is which element were going to be looking for event on
+    'submit .new-resolution': function (event) {
+      // .target targets the resolution form .value is the actual value of the field
+      var title = event.target.title.value;
+
+      // below is inserting the above info into the Collection(backend database)
+      Resolutions.insert({
+        // title is equal to the title which is the .value at above
+        title : title,
+        createdAt : new Date()
+      });
+
+      // clearing the form after you submit
+      event.target.title.value = "";
+      // return false does not refresh the page
+      return false;
+
+    }
+  });
+  // below references resolution template, not body because the delete button is in the template
+  Template.resolution.events({
+    // function for checking resolutions updating
+    'click .toggle-checked': function () {
+      // this._id takes the id, and changes the object inside.
+      // $set sets whatever we are going to change or modify, which is checked:
+      // !this.checked part is saying whatever boolean it is, make it opposite when clicked
+      Resolutions.update(this._id, {$set: {checked: !this.checked}})
+    }, //have to have comma here or it breaks...
+    // because we are looking for click event, that is the first part of the string
+    // .delete is because we are looking for the delete class button
+    'click .delete': function () {
+      // this._id will reference to whatever object id that is being clicked... mongo is awesome
+      Resolutions.remove(this._id);
+    }
+  });
+
+
+
+}
+// runs on server startup
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
